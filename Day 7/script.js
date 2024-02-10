641364ae813b16aa2bae84a5c35d0b566c143213
alicekkz <kakazu.haru@gmail.com>
2024-02-09 22:13:22 -0300

    notifications, search and menu (day 7)

---
+++ Day 7/script.js
@@ -0,0 +1,8 @@
+$('.search-icon').click(function() {
+    $('.search-input').togglesClass('active')
+})
+
+$('.menu-icon').click(function() {
+    $('.panel').toggleClass('show-menu');
+    $('.menu').toggleClass('active');
+})
